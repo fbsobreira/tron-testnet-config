5304a63fae3011cf4e7786e298b582455604c480
Fernando Sobreira <fbsobreira@gmail.com>
2018-06-04 16:22:50 -0400

    update witness file

---
+++ update_witness.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Ask for PK
+echo Please type node Private Key!
+read PK
+
+# repalce in file
+sed  "/PRIVATE_KEY=/c\PRIVATE_KEY=\"${PK}\"" work.sh.bkp > work.sh
+
+echo All done! now staret the server with \"./work.sh start\"
