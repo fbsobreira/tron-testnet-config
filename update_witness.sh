f56df119bed7222499e2be4e7a0246177e090359
Fernando Sobreira <fbsobreira@gmail.com>
2018-06-18 13:04:03 -0400

    get work.sh from submodule

--- update_witness.sh
+++ update_witness.sh
@@ -4,6 +4,6 @@ echo Please type node Private Key!
 read PK
 
 # repalce in file
-sed  "/PRIVATE_KEY=/c\PRIVATE_KEY=\"${PK}\"" work.sh.bkp > work.sh
+sed  "/PRIVATE_KEY=/c\PRIVATE_KEY=\"${PK}\"" ./java-tron/work.sh > ./work.sh
 
 echo All done! now staret the server with \"./work.sh start\"
